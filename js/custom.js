6c92f3cac87bcd5c76e6d11df0646121e2dfc0e1
Oscar Povea <oscaarpoveaa@gmail.com>
2022-11-21 14:29:44 +0100

    continuo

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,13 @@
 "use strict";
 
+function esconder() {
+  let fondo = document.getElementById("");
+  msg.classList.add("quitarmensaje");
+}
+
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  const modalToggle = document.getElementById('toggleMyModal'); 
+
+ document.getElementById("staticBackdrop").modal("show");
+});
